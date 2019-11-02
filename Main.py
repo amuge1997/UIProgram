295f9c3e56bc542313645cc534b66c9557759a1d
Amuge <amuge1997z@qq.com>
2019-11-02 19:07:17 +0800

    version 1: CAN: uichange AND recognition AND save. BUG: in save part in Task6.

--- Main.py
+++ Main.py
@@ -1,51 +1,5 @@
-#
-#
-# from PyQt5.QtCore import QThread, pyqtSignal
-# class SignalSlot(QThread):
-#     signal_setTextOupt = pyqtSignal(str)
-#     signal_setTextLabl = pyqtSignal(str, str)
-#     def __init__(self):
-#         QThread.__init__(self)
-#         self.signal_setTextOupt.connect(self.setTextEidt)
-#         self.signal_setTextLabl.connect(self.setTextLabe)
-#     def setTextEidt(self, sr):
-#         wid_textOupt = self.inst_QT.textOupt
-#         wid_textOupt.setStyleSheet("font-family:KaiTi;font-size:20px")
-#         wid_textOupt.setText(sr)
-#     def setTextLabe(self, sr, color):
-#         wid_textOupt = self.inst_QT.label
-#         wid_textOupt.setStyleSheet("color:{}".format(color))
-#         wid_textOupt.setText(sr)
-#
-# import queue, threading
-# class TaskParent:
-#     def __init__(self):
-#         self.que = queue.Queue()
-#         self.lc = threading.Lock()
-#         self.bl_listen = True
-#
-#     def slot(self):
-#         if self.bl_listen:
-#             self.lc.acquire()
-#             self.que.put({})
-#             self.lc.release()
-#             self.bl_listen = False
-#     def startListen(self):
-#         self.bl_listen = True
-#     def getSignal(self):
-#         self.lc.acquire()
-#         ret = self.que.get() if not self.que.empty() else False
-#         self.lc.release()
-#         return ret
-#     def cmdOutput(self, sr):
-#         print('>>> {}'.format(sr))
-
-
 try:
     from PyQt5.QtWidgets import *
-    # from ui import ui
-    # from ui2 import ui2
-    # from ui3 import ui3
     from ui2a3 import ui2a3
 
     try:
@@ -53,8 +7,6 @@ try:
     except:
         from WidgetManager import WidgetManager
 
-    # from Task1 import Task1
-    # from Task2 import Task2
     from Task4 import Task4
     from Task5 import Task5
     from Task6 import Task6
@@ -72,14 +24,6 @@ class Main(QMainWindow,WidgetManager,ui2a3):
         WidgetManager.__init__(self)
 
         self.setFixedSize(self.width(), self.height())
-
-        # self.Task1 = Task1(self)
-        # self.rc_inpu.textChanged.connect(self.Task1.slot)
-        # self.Task1.start()
-
-        # self.Task2 = Task2(self)
-        # self.pc_submit.clicked.connect(self.Task2.slot)
-        # self.Task2.start()
 
         self.regWidget(sr_widName='t4input',ins_widget=self.rc_inpu)
         self.regWidget(sr_widName='t4output',ins_widget=self.rc_oupt)
